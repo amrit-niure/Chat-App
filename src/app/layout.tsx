46fde68f5081ab8070d6f6ccddc136b2f85a837d
Amrit Niure <niureamrit2@gmail.com>
2023-08-11 11:36:10 +1000

    Before Real Time Functionality

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,9 @@
+import Providers from '@/components/Providers'
 import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ['latin'] ,weight : '400'})
 
 export const metadata: Metadata = {
   title: 'Create Next App',
@@ -15,8 +16,11 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
-    </html>
+    <html lang='en' className={inter.className}>
+    <body>
+      <Providers>{children}</Providers>
+    </body>
+  </html>
   )
 }
+
